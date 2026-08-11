cbf1dcd6fe0897b97b06beccc1192df508ff243d
riporippo <22fi124@ms.dendai.ac.jp>
2026-08-11 23:53:27 +0900

    初起動

--- kernel.c
+++ kernel.c
@@ -2,7 +2,13 @@ typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
-entern char __bss[], __bss_end[], __stack_top[];
+extern char __bss[], __bss_end[], __stack_top[];//シンボルのアドレスが知りたいので適当にChar型にしているらしい
+
+
+/* もしも extern char __bss;にすると
+実体を持たない変数であるのに、その中身を参照しようとしてしまう。
+→&演算子の付け忘れで予期せぬエラーが生じるから、[]にすることで&をつけずとも先頭アドレスを取得できる
+*/
 
 void *memset(void *buf, char c, size_t n){
 	uint8_t *p = (uint8_t *) buf;
@@ -18,12 +24,12 @@ void kernel_main(void){
 }
 
 __attribute__((section(".text.boot")))
-__attribute__((naked))
+__attribute__((naked)) // 関数冒頭、末尾のコードを書かないということ(boot関数より前の関数は存在しない→戻り先が存在せずパニックになるため)
 void boot(void){
 	__asm__ __volatile__(
-		"mv sp, %[stack_top]\n"
-		"j kernel_main\n"
+		"mv sp, %[stack_top]\n" //spにフレームポインタを代入
+		"j kernel_main\n" //問答無用でkernel_mainに遷移
 		:
-		: [stack_top] "r" (__stack_top)
-	)
+		: [stack_top] "r" (__stack_top) //リンカで定義されたフレームポインタ
+	);
 }
